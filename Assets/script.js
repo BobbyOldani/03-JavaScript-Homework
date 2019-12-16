4fd93ec78dc89623344d44d25dc4e890bc1edf00
Noisey Turtle <bobbyoldani2@gmail.com>
2019-12-16 15:10:13 -0800

    functioning PW generator now but no choices of characters

--- Assets/script.js
+++ Assets/script.js
@@ -1,18 +1,45 @@
-let specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "=", "]", "[", "?"];
-let numChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-let lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-let passLength = prompt("How many characters would you like your password to be? Your password must be between 8-128 characters!")
-let confSpec = confirm("Press OK if you would like special characters in your password");
-let confNum = confirm("Press OK if you would like numbers in your password");
-let confLower = confirm("Press OK if you would like lowercase letters in your password");
-let confUpper = confirm("Press OK if you would like uppercase letters in your password");
+//let charChoices
+//let specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "=", "]", "[", "?"];
+//let numChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//let lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//let upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-console.log(passLength);
-console.log(confSpec);
-console.log(confNum);
-console.log(confLower);
-console.log(confUpper);
+//let passLength = prompt("How many characters would you like your password to be? Your password must be between 8-128 characters!")
+//let confSpec = confirm("Press OK if you would like special characters in your password");
+//let confNum = confirm("Press OK if you would like numbers in your password");
+//let confLower = confirm("Press OK if you would like lowercase letters in your password");
+//let confUpper = confirm("Press OK if you would like uppercase letters in your password");
+
+//console.log(passLength);
+//console.log(confSpec);
+//console.log(confNum);
+//console.log(confLower);
+//console.log(confUpper);
+
+//---------------------------------------------------------------------------
+function generate(){
+    let passLength = prompt("How many characters would you like your password to be? Your password must be between 8-128 characters!");
+    let values = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()=+[]?";
+    let password = "";
+
+
+    for (let i = 0; i <= passLength; i++){
+        password = password + values.charAt(Math.floor(Math.random() *Math.floor(values.length -1)));
+        
+        }
+
+        document.getElementById("securepass").innerHTML = password;
+
+        console.log(password);
+
+        }
+
+function clipboard(){
+    let copyClip = document.getElementById("securepass");
+    copyClip.select();
+    copyClip.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+}
 
 
