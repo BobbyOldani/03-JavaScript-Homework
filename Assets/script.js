f693f8b0fab92d7a49e74c1b7feb91efe70227ac
Noisey Turtle <bobbyoldani2@gmail.com>
2019-12-17 15:30:03 -0800

    re designed javascript that includes the options for different sets of char

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,3 @@
-
-
 // let specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "=", "]", "[", "?"];
 // let numChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 // let lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -23,36 +21,66 @@
 //console.log(confUpper);
 
 //---------------------------------------------------------------------------
-function generate(){
-    let passLength = prompt("How many characters would you like your password to be? Your password must be between 8-128 characters!");
-    let values = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()=+[]?";
+function generate() {
+    let passLength = prompt(
+      "How many characters would you like your password to be? Your password must be between 8-128 characters!"
+    );
+    let values = [
+      "!@#$%^&*()=+[]?",
+      "0123456789",
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      "abcdefghijklmnopqrstuvwxyz"
+    ];
+  
     let password = "";
-
-
-    for (let i = 0; i <= passLength; i++){
-        if (passLength >= 8 && passLength <= 128){
-
-            password = password + values.charAt(Math.floor(Math.random() *Math.floor(values.length -1)));
+    let options = [
+      "inlineCheckbox1", //Special Characters
+      "inlineCheckbox2", //Numbers
+      "inlineCheckbox3", //Uppercase
+      "inlineCheckbox4"  //Lowercase
+    ];
+    function getRandomInt(max) { //creates a function to get a random number                                     
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+    if (passLength >= 8 && passLength <= 128) {
+      let allUnchecked = true;
+      for (option of options) {
+        if (document.getElementById(option).checked) {
+          allUnchecked = false;
         }
-        
-        else {
-            alert("I SAID BETWEEN 8-128!... CHOOSE WISELY")
+      }
+      if (!allUnchecked) {
+        for (let i = 0; i <= passLength; i++) {
+          //seed holds the value for what type of character we will generate
+          let seed;
+          do {
+            seed = getRandomInt(options.length); //randomly select a character type
+            console.log("seed: ", seed);
+            console.log(document.getElementById(options[seed]).checked);
+          } while (!document.getElementById(options[seed]).checked); //check to see if that type was selected otherwise choose again
+  
+          password =
+            password +
+            values[seed].charAt(
+              Math.floor(Math.random() * Math.floor(values[seed].length - 1))
+            );
         }
-
-        }
-        
-        document.getElementById("securepass").value = password;
-
-        console.log(password);
-
-        }
-
-function clipboard(){
+      }
+      else {
+        alert("CHECK A DAMN BOX!");
+      }
+      // check to see whether password length is valid
+    } else {
+      alert("I SAID BETWEEN 8-128!... CHOOSE WISELY");
+    }
+  
+    document.getElementById("securepass").value = password;
+    console.log(password);
+  }
+  
+  function clipboard() {
     let copyClip = document.getElementById("securepass");
-    copyClip.focus();
     copyClip.select();
     document.execCommand("copy");
-}
-
-
-
+  }
+  
